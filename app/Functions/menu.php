7f9649f54db9deb3c40a7c40b8dceba0ba07b99b
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2022-04-28 12:29:01 +0200

    Added caching for menus

--- app/Functions/menu.php
+++ app/Functions/menu.php
@@ -1,8 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+
 if( !function_exists( 'menu' ) ) {
     function menu( $sGroup = 'guest', $depth = 0, $parent = 0 ) {
         $oRender = new \App\Render\Menu();
-        return $oRender->buildMenu( $parent, $depth, $sGroup );
+
+        if( $sMenu = Cache::get( 'menu_' . $sGroup . '_' . $depth . '_' . $parent ) ) {
+            // $sMenu = Cache::get( 'menu_' . $sGroup . '_' . $depth . '_' . $parent );
+            echo $sMenu;
+            return;
+        } else {
+            ob_start();
+            $oRender->buildMenu( $parent, $depth, $sGroup );
+            $sMenu = ob_get_clean();
+
+            Cache::put( 'menu_' . $sGroup . '_' . $depth . '_' . $parent, $sMenu, 60 );
+        }
+
+        echo $sMenu;
+        return;
     }
 }
